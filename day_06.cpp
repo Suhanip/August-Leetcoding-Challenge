776ac9a83b966bbe4045ded220082de7d8ae0edb
Suhanip <kushan0342@gmail.com>
2020-09-03 00:10:03 +0530

    find all the duplicates in array

---
+++ day_06.cpp
@@ -0,0 +1,13 @@
+class Solution {
+public:
+    vector<int> findDuplicates(vector<int>& nums) {
+        vector <int> result;
+        for(int n : nums){
+            n = abs(n);
+            //number appears for first time, so make it negative
+            if(nums[n-1] > 0) nums[n-1] *= -1;
+            else result.push_back(n);
+        }
+      return result;
+    }
+};
